5da57e07ea5cdfad3e75f311dcb229892a804560
Michelle Shaia <michelle.s@nanox.vision>
2024-02-06 02:00:26 +0200

    improvements

--- src/LanesDetectionOnFrame.py
+++ src/LanesDetectionOnFrame.py
@@ -4,8 +4,20 @@ import matplotlib.pyplot as plt
 
 
 class LanesDetectionOnFrame:
-    CENTER_LINE_X = 417
-    OFFSET = 5
+    LEFT_ANNOTATION = "Left <-"
+    RIGHT_ANNOTATION = "Right ->"
+
+    def __init__(self, center_line_x, offset, frame_rate_per_second):
+        self.center_lane_x = 410
+        self.y_depth_search = 386
+        self.right_lane_x = 670
+        self.left_lane_x = 180
+        self.offset = offset
+        self.frame_rate_per_second = frame_rate_per_second
+        self.crossing_roads = False
+        self.counter_frame = 0
+        self.crossing_roads_lines = []
+        self.cross_direction = None
 
     @staticmethod
     def preprocess_image(image):
@@ -31,42 +43,36 @@ class LanesDetectionOnFrame:
         return morph_image
 
     @staticmethod
-    def separate_lines(lines):
-        left_lines = []
-        right_lines = []
-
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            slope = (y2 - y1) / (x2 - x1) if (x2 - x1) != 0 else 999  # Avoid division by zero
-
-            # Threshold for considering a line as part of the left or right lane
-            if abs(slope) < 0.3:  # Ignore lines with steep slopes (near vertical)
-                continue
-
-            if slope < 0:
-                left_lines.append(line)
-            else:
-                right_lines.append(line)
-
-        return left_lines, right_lines
-
+    def slope(x1, y1, x2, y2):
+        return (y2 - y1) / (x2 - x1) if (x2 - x1) != 0 else 999  # Avoid division by zero
 
     @staticmethod
     def find_x_on_line(line, y3):
         x1, y1, x2, y2 = line
-        slope = (y2 - y1) / (x2 - x1) if (x2 - x1) != 0 else 999  # Avoid division by zero
+        slope = LanesDetectionOnFrame.slope(x1, y1, x2, y2)
         y_intercept = y1 - slope * x1
 
         x3 = int((y3 - y_intercept) / slope) if slope != 0 else x1
         return x3
 
     @staticmethod
-    def find_most_populous_line(image, lines, y_bottom):
+    def find_most_populous_line(image, lines, orientation="right"):
         most_populous_line = None
         max_points = 0
 
         for line in lines:
             x1, y1, x2, y2 = line[0]
+
+            slope = LanesDetectionOnFrame.slope(x1, y1, x2, y2)
+
+            if abs(slope) < 0.4:  # Ignore lines with steep slopes (near vertical)
+                continue
+
+            if orientation == "right" and slope < 0:  # Ignore lines with negative slopes
+                continue
+            elif orientation == "left" and slope > 0:  # Ignore lines with positive slopes
+                continue
+
             points_on_line = np.sum(cv2.line(np.zeros_like(image), (x1, y1), (x2, y2), (0, 0, 255), 1))
 
             if points_on_line > max_points:
@@ -87,55 +93,101 @@ class LanesDetectionOnFrame:
         return extended_line
 
     @staticmethod
-    def find_lane_lines(image):
-        processed_image = LanesDetectionOnFrame.preprocess_image(image)
+    def calculate_mask(roi, edge_image):
+        mask = np.zeros_like(edge_image)
+        cv2.fillPoly(mask, [np.array(roi)], 255)
+        plt.imshow(mask)
+        plt.show()
+        mask_edge = cv2.bitwise_and(edge_image, mask)
+        plt.imshow(mask_edge)
+        plt.show()
+        return mask_edge
+
+    def find_lane_lines(self, image):
+        processed_image = self.preprocess_image(image)
         # Edge Detection
         edges = cv2.Canny(processed_image, 50, 150)
         height, width = edges.shape
-
-        roi_left_vertices = [(230, height),
-                             (LanesDetectionOnFrame.CENTER_LINE_X - LanesDetectionOnFrame.OFFSET, height),
-                             (LanesDetectionOnFrame.CENTER_LINE_X - LanesDetectionOnFrame.OFFSET, 360)]
-
-        mask_left = np.zeros_like(edges)
-        cv2.fillPoly(mask_left, [np.array(roi_left_vertices)], 255)
-
-        masked_edges_left = cv2.bitwise_and(edges, mask_left)
-        plt.imshow(masked_edges_left)
-        plt.show()
-
-        # Hough Transform for right mask
-
-        roi_right_vertices = [(LanesDetectionOnFrame.CENTER_LINE_X + LanesDetectionOnFrame.OFFSET, height),
-                              (700, height), (LanesDetectionOnFrame.CENTER_LINE_X + LanesDetectionOnFrame.OFFSET, 360)]
-
-        mask_right = np.zeros_like(edges)
-        cv2.fillPoly(mask_right, [np.array(roi_right_vertices)], 255)
-
-        masked_edges_right = cv2.bitwise_and(edges, mask_right)
-        plt.imshow(masked_edges_right)
-        plt.show()
-
-        # Hough Transform for left mask
-        lines_right = cv2.HoughLinesP(masked_edges_right, 6, np.pi / 180, threshold=50, minLineLength=60,
-                                      maxLineGap=200)
-        lines_left = cv2.HoughLinesP(masked_edges_left, 6, np.pi / 90, threshold=50, minLineLength=60, maxLineGap=200)
-
-        roi_vertices = [(230, height), (700, height), (height - 80, 360)]
-
-        # mask = np.zeros_like(edges)
-
-        # Average Lines and Extend to the Bottom
         y_bottom = height
         most_populous_left_line = None
         most_populous_right_line = None
+
+        roi_left_vertices = [(self.left_lane_x, height),
+                             (self.center_lane_x - 1, height),
+                             (self.center_lane_x - 1, self.y_depth_search),
+                             (self.center_lane_x - 1 - self.offset, self.y_depth_search)]
+
+        roi_right_vertices = [(self.right_lane_x, height),
+                              (self.center_lane_x + 1, height),
+                              (self.center_lane_x + 1, self.y_depth_search),
+                              (self.center_lane_x + 1 + self.offset, self.y_depth_search)]
+
+        masked_edges_left = self.calculate_mask(roi_left_vertices, edges)
+
+        masked_edges_right = self.calculate_mask(roi_right_vertices, edges)
+
+        lines_right = cv2.HoughLinesP(masked_edges_right, 6, np.pi / 90, threshold=25, minLineLength=25, maxLineGap=200)
+        lines_left = cv2.HoughLinesP(masked_edges_left, 6, np.pi / 90, threshold=25, minLineLength=25, maxLineGap=200)
+
+        if lines_left is None:
+            print("left line not found")
+        if lines_right is None:
+            print("right line not found")
+
         if lines_left is not None:
-            most_populous_left_line = LanesDetectionOnFrame.find_most_populous_line(image, lines_left, y_bottom)
-            most_populous_left_line = LanesDetectionOnFrame.extend_line(most_populous_left_line[0], y_bottom)
+            self.crossing_roads = False
+            self.crossing_roads_lines = []
+            most_populous_left_line = self.find_most_populous_line(image, lines_left, orientation="left")
+            if most_populous_left_line is not None:
+                most_populous_left_line = self.extend_line(most_populous_left_line[0], y_bottom)
 
         if lines_right is not None:
-            most_populous_right_line = LanesDetectionOnFrame.find_most_populous_line(image, lines_right, y_bottom)
-            most_populous_right_line = LanesDetectionOnFrame.extend_line(most_populous_right_line[0], y_bottom)
+            self.crossing_roads = False
+            self.crossing_roads_lines = []
+            most_populous_right_line = self.find_most_populous_line(image, lines_right, orientation="right")
+            if most_populous_right_line is not None:
+                most_populous_right_line = self.extend_line(most_populous_right_line[0], y_bottom)
+
+        # possible change lane scenario
+        # if lines_right is None and lines_left is None:
+        #     roi_middle_vertices = [(350, height),
+        #                            (370, 390),
+        #                            (520, 390),
+        #                            (550, height)
+        #                            ]
+        #
+        #     masked_edges_middle = self.calculate_mask(roi_middle_vertices, edges)
+        #
+        #     lines_middle = cv2.HoughLinesP(masked_edges_middle, 6, np.pi / 180, threshold=30, minLineLength=30,
+        #                                    maxLineGap=200)
+        #     if lines_middle is not None:
+        #         most_populous_middle_line = self.find_most_populous_line(image, lines_middle)
+        #         if most_populous_middle_line is not None:
+        #             self.crossing_roads = True
+        #             self.counter_frame = 0
+        #             most_populous_middle_line = self.extend_line(most_populous_middle_line[0],
+        #                                                          y_bottom)
+        #             self.crossing_roads_lines.append(most_populous_middle_line)
+        #             x1, y1, x2, y2 = most_populous_middle_line
+        #             cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 8)
+        #
+        # if len(self.crossing_roads_lines) == 5:
+        #     x1, y1, x2, y2 = self.crossing_roads_lines[0]
+        #     x3, y3, x4, y4 = self.crossing_roads_lines[4]
+        #     if x1 < x3:
+        #         self.cross_direction = self.LEFT_ANNOTATION
+        #     else:
+        #         self.cross_direction = self.RIGHT_ANNOTATION
+        #
+        # if self.cross_direction or 0 < self.counter_frame < self.frame_rate_per_second:
+        #     cv2.putText(image, f"Changing lane to {self.cross_direction}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+        #                 (0, 0, 255), 2,
+        #                 cv2.LINE_AA)
+        #     self.counter_frame += 1
+        #
+        # if self.counter_frame == self.frame_rate_per_second:
+        #     self.counter_frame = 0
+        #     self.cross_direction = None
 
         # Draw Lane Lines on Original Image
         if most_populous_left_line is not None:
--- src/lane_detection1.py
+++ src/lane_detection1.py
@@ -8,6 +8,8 @@ input_directory = '../input_videos/'
 output_directory = '../annotated_videos/'
 
 # Read the video file
+#video_filename = 'change_lane_to_right.mp4'
+
 video_filename = 'cropped_change_lines_low_quality.mp4'
 video_path = input_directory + video_filename
 
@@ -19,10 +21,12 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec and create VideoWriter object
-output_filename = f"annotated_video_1.mp4"
+output_filename = f"video1.mp4"
 output_path = output_directory + output_filename
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+lanesDetectionOnFrame = LanesDetectionOnFrame(center_line_x=417, offset=80, frame_rate_per_second=fps)
 
 while cap.isOpened():
     # Step 1: Read Frame from capture
@@ -30,7 +34,7 @@ while cap.isOpened():
     if not ret:
         break
 
-    line_image = LanesDetectionOnFrame.find_lane_lines(frame)
+    line_image = lanesDetectionOnFrame.find_lane_lines(frame)
 
     # Step 8: Drawing the Lines
     final_image = cv2.addWeighted(frame, 0.8, line_image, 1, 0)
