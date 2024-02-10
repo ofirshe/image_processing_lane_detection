6c36bb7813482e96c8eba361abb6f9bc4f861f30
Michelle Shaia <michelle.s@nanox.vision>
2024-02-10 15:22:26 +0200

    add annotated videos

--- src/main.py
+++ src/main.py
@@ -44,46 +44,46 @@ if __name__ == '__main__':
     output_directory = '../annotated_videos/'
 
     # 1. lanes changing
-    # video_input_filename = 'change_lanes.mp4'
-    # video_output_filename = f"{video_input_filename.split('.')[0]}_annotated.mp4"
-    # vid1_param_dict = {
-    #     'center_line_x': 425,
-    #     'y_depth_search': 386,
-    #     'right_lane_x': 670,
-    #     'left_lane_x': 190,
-    #     'offset': 80,
-    #     'frame_rate_per_second': 30,
-    #     'saturation_threshold': 120,
-    #     'rho': 6,
-    #     'theta': np.pi / 90,
-    #     'min_line_length': 25,
-    #     'debug': True
-    # }
-    #
-    # vid1_lane_detection = LanesDetectionOnFrame(**vid1_param_dict)
-    # record_movie(input_directory + video_input_filename, output_directory + video_output_filename,
-    #              vid1_lane_detection)
-    #
-    # # 2. Night-Time Lane Detection
-    # video_input_filename = 'night_ride.mp4'
-    # video_output_filename = f"{video_input_filename.split('.')[0]}_annotated.mp4"
-    # vid2_param_dict = {
-    #     'center_line_x': 434,
-    #     'y_depth_search': 350,
-    #     'right_lane_x': 706,
-    #     'left_lane_x': 218,
-    #     'offset': 80,
-    #     'frame_rate_per_second': 30,
-    #     'saturation_threshold': 170,
-    #     'rho': 6,
-    #     'theta': np.pi / 90,
-    #     'min_line_length': 25,
-    #     'debug': True
-    # }
-    #
-    # vid2_lane_detection = LanesDetectionOnFrame(**vid2_param_dict)
-    # record_movie(input_directory + video_input_filename, output_directory + video_output_filename,
-    #              vid2_lane_detection)
+    video_input_filename = 'change_lanes.mp4'
+    video_output_filename = f"{video_input_filename.split('.')[0]}_annotated.mp4"
+    vid1_param_dict = {
+        'center_line_x': 425,
+        'y_depth_search': 386,
+        'right_lane_x': 670,
+        'left_lane_x': 190,
+        'offset': 80,
+        'frame_rate_per_second': 30,
+        'saturation_threshold': 120,
+        'rho': 6,
+        'theta': np.pi / 90,
+        'min_line_length': 25,
+        'debug': True
+    }
+
+    vid1_lane_detection = LanesDetectionOnFrame(**vid1_param_dict)
+    record_movie(input_directory + video_input_filename, output_directory + video_output_filename,
+                 vid1_lane_detection)
+
+    # 2. Night-Time Lane Detection
+    video_input_filename = 'night_ride.mp4'
+    video_output_filename = f"{video_input_filename.split('.')[0]}_annotated.mp4"
+    vid2_param_dict = {
+        'center_line_x': 434,
+        'y_depth_search': 350,
+        'right_lane_x': 706,
+        'left_lane_x': 218,
+        'offset': 80,
+        'frame_rate_per_second': 30,
+        'saturation_threshold': 170,
+        'rho': 6,
+        'theta': np.pi / 90,
+        'min_line_length': 25,
+        'debug': True
+    }
+
+    vid2_lane_detection = LanesDetectionOnFrame(**vid2_param_dict)
+    record_movie(input_directory + video_input_filename, output_directory + video_output_filename,
+                 vid2_lane_detection)
 
     # 3. distance approximation detection
     video_input_filename = 'distance_detection_2.mp4'
